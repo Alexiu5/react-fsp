1d1780855371ee2a7bbe01e1aa48950a6c2a9cd8
Alex <johan.higuita@quipux.com>
2019-03-11 11:59:13 -0500

    add loading state

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -5,28 +5,23 @@ import * as constants from '../../constants/weather'
 import transformWeather from '../../services/transformWeather'
 import {api_weater} from '../../constants/api_url'
 
-const data = {
-    temperature : 5,
-    weatherState :constants.SUNNY,
-    humidity: 10,
-    wind: '10 m/s'
-}
-
 class WeatherLocation extends Component {
 
     constructor(){
         super();
         this.state = {
-            city : 'Medellín',
-            data: data
+            city : 'Loading...',
+            data: null
         }
     }
 
-    handleUpdateClick = ()=>{
+    componentDidMount() {
         fetch(api_weater).then(response => response.json())
         .then(response =>{
             let newWeather = transformWeather(response)       
+            let {name} = response
             this.setState({
+                city : name,
                 data : newWeather
             })
         })
@@ -34,14 +29,14 @@ class WeatherLocation extends Component {
             console.error(error)
         })
     }
-
+    
     render(){ 
         const  {city, data}  = this.state
         return (
+            
             <div>
                 <h1>{city}</h1>
-                <WeatherData data={data}></WeatherData>
-                <button onClick={this.handleUpdateClick}>Actualiar</button>
+                {data ? <WeatherData data={data}></WeatherData> : ""}
             </div>
         );
     }
--- src/constants/api_url.js
+++ src/constants/api_url.js
@@ -1,4 +1,4 @@
-export const location = 'London'
+export const location = 'San Francisco, us'
 export const api_key = '85c426a7a9490e80f373a95533e1ac25'
 export const url_base_weather = 'http://api.openweathermap.org/data/2.5/weather'
 
